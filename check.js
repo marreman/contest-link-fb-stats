2814d885cd261dfb07b864fd7116e33f518c94d7
Martin Liberg <mr.liberg@gmail.com>
2014-06-03 18:06:45 +0200

    initial commit

---
+++ check.js
@@ -0,0 +1,95 @@
+var request = require("request")
+  , parseString = require("xml2js").parseString
+  , q = require('q')
+  , urls =  {
+      fb: "https://api.facebook.com/restserver.php?method=links.getStats&urls=%urls",
+      contest: "http://contest.theamazingsociety.com/_fa/contest/%contestKey",
+      getEntries: "http://contest.theamazingsociety.com/_fa/contest_entries?filter=contest_key.eq(%contestKey)",
+      entry: "http://contest.theamazingsociety.com/%contestKey/contributions/%entryKey"
+  };
+
+
+function getContest(contestKey) {
+  var deferred = q.defer();
+
+  console.log('getContest', contestKey);
+
+  request(urls.contest.replace('%contestKey', contestKey), function(error, response, body) {
+    deferred.resolve(JSON.parse(body));
+  });
+
+  return deferred.promise;
+}
+
+function getEntries(contest) {
+  var deferred = q.defer();
+
+  console.log('getEntries', contest.key);
+
+  request(urls.getEntries.replace('%contestKey', contest.key), function(error, response, body) {
+    var url = urls.entry.replace('%contestKey', contest.key_name)
+      , result;
+
+    result = JSON.parse(body).map(function (entry) {
+      return url.replace('%entryKey', entry.key_name);
+    });
+
+    deferred.resolve(result);
+  });
+
+  return deferred.promise;
+}
+
+function getLinkDataFromFacebook(entryUrls) {
+  var deferred = q.defer();
+
+  console.log('getLinkDataFromFacebook', entryUrls.length);
+
+  request(urls.fb.replace('%urls', entryUrls.join(',')), function(error, response, body) {
+    deferred.resolve(body);
+  });
+
+  return deferred.promise;
+}
+
+function xml2json(xml) {
+  var deferred = q.defer();
+
+  console.log('xml2json', xml.length);
+
+  parseString(xml, function (err, json) {
+    deferred.resolve(json);
+  });
+
+  return deferred.promise;
+}
+
+function summarize(result) {
+  var stats = result.links_getStats_response.link_stat
+    , result = { likes: 0, comments: 0, shares: 0, total: 0 }
+    , deferred = q.defer();
+
+  stats.forEach(function (stat) {
+    result.likes += parseInt(stat.like_count.pop(), 10);
+    result.comments += parseInt(stat.comment_count.pop(), 10);
+    result.shares += parseInt(stat.share_count.pop(), 10);
+    result.total += parseInt(stat.total_count.pop(), 10);
+  });
+
+  deferred.resolve(result);
+
+  return deferred.promise;
+}
+
+
+var contestKey = 'ahFlfmFtYXppbmctY29udGVzdHIUCxIHQ29udGVzdCIHbjlkOW1rYQw';
+
+getContest(contestKey)
+  .then(getEntries)
+  .then(getLinkDataFromFacebook)
+  .then(xml2json)
+  .then(summarize)
+  .then(function (result) {
+    console.log(result);
+  });
+
